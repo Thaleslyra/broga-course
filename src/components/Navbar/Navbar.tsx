e1887775cf3072f6d58dc99f362c3b959eb1c146
THALES ANDRÉ DA SILVA LYRA <thalesalyra@gmail.com>
2024-08-22 17:20:40 -0300

    Added Icons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,60 @@
 import React from "react";
+import {FaceHappy, GamepadIcon, HomeIcon, MedalIcon} from "@/components";
+import {green} from "next/dist/lib/picocolors";
+import {RouteIcon} from "@/components/Icons/Icons/RouteIcon";
+
+const NavBarList = ({children, className}) => {
+    return (
+        <ul className={`my-4 border-t  border-indigo-400/20 hover:border-indigo-400/40 ${className}`}>
+            {children}
+        </ul>
+    )
+}
+
+const NavBarLisItem = ({children}) => {
+    return (
+        <li className="my-2 p-2 rounded-lg bg-transparent hover:bg-indigo-400/40 cursor-pointer flex gap-2 items-center hover:text-slate-100">
+            {children}
+        </li>
+
+    )
+}
 
 export default function Navbar() {
     return (
         <nav
-            className=" flex flex-col ga-4 h-screen bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72">
-            <div> logo<img src="" alt="" className="max-w-full p-2"/></div>
-            {/*colocar a logo*/}
-            <ul className=" flex-grow my-4 border-t  border-indigo-400/20 hover:border-indigo-400/40">
-                <li className="my-2 p-2 rounded-lg bg-transparent hover:bg-slate-800 cursor-pointer">Home</li>
-                <li className="my-2 p-2 rounded-lg bg-transparent hover:bg-slate-800 cursor-pointer">Games</li>
-                <li className="my-2 p-2 rounded-lg bg-transparent hover:bg-slate-800 cursor-pointer">Top 10</li>
-                <li className="my-2 p-2 rounded-lg bg-transparent hover:bg-slate-800 cursor-pointer">Walkthroughs</li>
-            </ul>
-            <ul className="my-4 border-t  border-indigo-400/20 hover:border-indigo-400/40">
-                <li className="my-2 p-2 rounded-lg bg-transparent hover:bg-slate-800 cursor-pointer">User</li>
-            </ul>
+            className=" flex flex-col  h-screen bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 text-slate-300">
+            <div className="flex items-center justify-center m-4"><img
+                src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png" alt=""
+                className="w-auto h-12 p-2"/></div>
+
+
+            <NavBarList className="flex-grow">
+                <NavBarLisItem>
+                    <HomeIcon className=" w-4 h-4 "/> Home
+                </NavBarLisItem>
+
+                <NavBarLisItem>
+                    <GamepadIcon className="w-4 h-4"/> Games
+                </NavBarLisItem>
+
+                <NavBarLisItem>
+                    <MedalIcon className="w-4 h-4"/> Top 10
+                </NavBarLisItem>
+
+                <NavBarLisItem>
+                    <RouteIcon className="w-4 h-4"/> Walkthroughs
+                </NavBarLisItem>
+
+            </NavBarList>
+
+            <NavBarList className={""}>
+                <NavBarLisItem>
+                    <FaceHappy className="w-4 h-4"/>User
+                </NavBarLisItem>
+
+            </NavBarList>
+
         </nav>
     )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
+import { HomeIcon} from "@/components/Icons/Icons/HomeIcon";
+import { GamepadIcon} from "@/components/Icons/Icons/GamepadIcon";
+import { MedalIcon} from "@/components/Icons/Icons/MedalIcon";
+import { FaceHappy } from "./Icons/Icons/FaceHappy";
 
+export { HomeIcon}
+export { MedalIcon}
+export { GamepadIcon}
 export { Navbar}
+export { FaceHappy }
